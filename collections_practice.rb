1ae5b5e4d9da1c402fd00c8123def7a323c137b7
Leslie Turis <lturis@gmail.com>
2018-09-12 20:07:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -74,6 +74,28 @@ def find_cool(array)
 
 end
 
+:schools = {
+  {
+    "flatiron school bk" => {
+      :location => "NYC"
+    },
+    "flatiron school" => {
+      :location => "NYC"
+    },
+    "dev boot camp" => {
+      :location => "SF"
+    },
+    "dev boot camp chicago" => {
+      :location => "Chicago"
+    },
+    "general assembly" => {
+      :location => "NYC"
+    },
+    "Hack Reactor" => {
+      :location => "SF"
+    }
+  }
+}
 
 
 
